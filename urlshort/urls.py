20a339bcd025f4c8424e48384d06437c31ab8164
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-24 08:26:53 +0300

    creating remaining html templates and URL Patterns

--- urlshort/urls.py
+++ urlshort/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('create/', createShortURL, name='create'),
+    path('<str:url>/', redirect, name='redirect'),
 ]
--- urlshort/views.py
+++ urlshort/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'urlshort/home.html')
+
+
+def createShortURL(request):
+    pass
+
+
+def redirect(request):
+    pass
+
+
+
+
+
+
+
+
+
+
+
