2d54ca22ba1b56ac5f77d70f6abd954829469899
Alexander Waya Nguku <alex.waya@gmail.com>
2023-01-24 10:11:07 +0300

    creating the redirec view

--- urlshort/views.py
+++ urlshort/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 
+from .models import ShortURL
+
 
 def home(request):
     return render(request, 'urlshort/home.html')
 
+def redirect(request, url):
+    current_obj = ShortURL.objects.filter(short_url=url)
+    if len(current_obj) == 0:
+        return render(request, 'urlshort/pagenotfound.html')
+    context = {
+        'obj':current_obj[0],
+    }
+    return render(request, 'urlshort/redirect.html', context)
 
 def createShortURL(request):
     pass
 
 
-def redirect(request):
-    pass
+
 
 
 
